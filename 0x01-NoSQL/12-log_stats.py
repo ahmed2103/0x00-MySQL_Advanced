060e84d1a70f6f48d71258b2250b769a99e759a0
Hasona <bekoahmed20@gmail.com>
2024-07-16 18:01:18 +0300

    refactor test

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,7 +2,18 @@
 """Script that provides some stats about Nginx logs stored in MongoDB:"""
 
 
+def logs_methods_print(nginx_col):
+    """function that prints nginx logs stored in MongoDB and methods"""
+    cnt = nginx_col.count_documents({})
+    print('{} logs'.format(cnt), 'Methods:', sep='\n')
 
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        cnt_method = nginx_col.count_documents({"method": method})
+        print('\tmethod {}: {}'.format(method, cnt_method))
+
+    print('{} status check'.format(
+        nginx_col.count_documents({
+            "method": "GET", "path": "/status"})))
 
 
 if __name__ == "__main__":
