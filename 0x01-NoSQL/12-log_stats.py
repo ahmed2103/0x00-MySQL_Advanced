5a3346d6a6410016904ff087ee63f3d511fc0612
Hasona <bekoahmed20@gmail.com>
2024-07-16 17:48:06 +0300

    refactor test

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 """Script that provides some stats about Nginx logs stored in MongoDB:"""
 
-from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017/')
-    nginx_logs = client.logs.nginx
-    cnt = nginx_logs.count_documents({})
+def logs_methods_print(nginx_col):
+    """function that prints nginx logs stored in MongoDB and methods"""
+    cnt = nginx_col.count_documents({})
     print('{} logs'.format(cnt), 'Methods:', sep='\n')
 
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        cnt_method = nginx_logs.count_documents({"method": method})
+        cnt_method = nginx_col.count_documents({"method": method})
         print('\tmethod {}: {}'.format(method, cnt_method))
 
-    print('{} status check'
-          .format(nginx_logs.count_documents(
-            {"method": "GET", "path": "/status"})))
+    print('{} status check'.format(
+        nginx_col.count_documents({
+            "method": "GET", "path": "/status"})))
+
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    client = MongoClient('mongodb://localhost:27017/').logs.nginx
+    logs_methods_print(client)
